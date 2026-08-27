f70132fdc06ebc26c8242bd037202f75b8a0f04f
Hillary Guevara <hillaryguevarasegura@gmail.com>
2026-08-26 23:59:27 -0400

    14: vender producto y control de stock

--- 14-ProyectoFinal/Program.cs
+++ 14-ProyectoFinal/Program.cs
@@ -8,7 +8,8 @@ do
     Console.WriteLine("\n=== La Repostería de Hill ===");
     Console.WriteLine("1. Agregar producto");
     Console.WriteLine("2. Ver inventario");
-    Console.WriteLine("3. Salir");
+    Console.WriteLine("3. Vender producto");
+    Console.WriteLine("4. Salir");
     Console.WriteLine("Elige una opción:");
     opcion = Convert.ToInt32(Console.ReadLine());
 
@@ -23,6 +24,7 @@ do
             int cant = Convert.ToInt32(Console.ReadLine());
             AgregarProducto(nom, precio, cant);
             break;
+
         case 2:
             if (nombres.Count == 0)
             {
@@ -37,14 +39,40 @@ do
                 }
             }
             break;
+
         case 3:
+            Console.WriteLine("Nombre del producto a vender:");
+            string productoVender = Console.ReadLine();
+            int indice = nombres.IndexOf(productoVender);
+            if (indice == -1)
+            {
+                Console.WriteLine("Producto no encontrado.");
+            }
+            else
+            {
+                Console.WriteLine($"Stock actual: {cantidades[indice]}. ¿Cuánto deseas vender?");
+                int cantVender = Convert.ToInt32(Console.ReadLine());
+                if (cantVender <= cantidades[indice])
+                {
+                    cantidades[indice] -= cantVender;
+                    Console.WriteLine($"Venta realizada. Nuevo stock de {nombres[indice]}: {cantidades[indice]}");
+                }
+                else
+                {
+                    Console.WriteLine("Error: Stock insuficiente para realizar la venta.");
+                }
+            }
+            break;
+
+        case 4:
             Console.WriteLine("Saliendo...");
             break;
+
         default:
             Console.WriteLine("Opción inválida");
             break;
     }
-} while (opcion != 3);
+} while (opcion != 4);
 
 void AgregarProducto(string nombre, double precio, int cantidad)
 {
