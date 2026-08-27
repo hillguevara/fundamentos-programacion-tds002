866bd0983d3c1e892588630862677404d4444040
Hillary Guevara <hillaryguevarasegura@gmail.com>
2026-08-26 22:13:21 -0400

    01: Hola Mundo y presentación

---
+++ 01-HolaMundo/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Nombre: Hillary Guevara Segura
+   Fecha: 26/08/2026
+   Descripción: Tarjeta de presentación en consola */
+
+// Imprime el marco superior
+Console.WriteLine("========================");
+
+// Imprime mis datos personales
+Console.WriteLine("| Hillary Guevara Segura |");
+Console.WriteLine("| Matrícula: 20260946   |");
+Console.WriteLine("| Carrera: Desarrollo de software |");
+Console.WriteLine("| Todo es parte del proceso        |");
+
+// Imprime el marco inferior
+Console.WriteLine("========================");
