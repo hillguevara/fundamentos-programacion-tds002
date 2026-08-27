491fb54251df1f39f95bc5b8b1d800426f397b14
Hillary Guevara <hillaryguevarasegura@gmail.com>
2026-08-26 23:18:06 -0400

    07: ciclos while, do-while y for

---
+++ 07-Ciclos/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Escribe un número:");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= 12; i++)
+{
+    Console.WriteLine($"{numero} x {i} = {numero * i}");
+}
+
+Console.WriteLine("\n--- break y continue ---");
+for (int i = 1; i <= 50; i++)
+{
+    if (i % 3 == 0)
+    {
+        continue;
+    }
+    if (i % 7 == 0 && i > 30)
+    {
+        break;
+    }
+    Console.WriteLine(i);
+}
